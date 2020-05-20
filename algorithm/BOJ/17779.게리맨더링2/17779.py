24255b7cd63b0095b00e4bca2b8b617e827ab815
wally-wally <wallys0213@gmail.com>
2020-05-21 00:25:10 +0900

    200521 | revise BOJ 17779 problem

--- algorithm/BOJ/17779.게리맨더링2/17779.py
+++ algorithm/BOJ/17779.게리맨더링2/17779.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('input_17779.txt', 'r')
 
 def calc_gerrymandering(r, c, max_d1, max_d2):
-    temp_answer = 0xffffff
+    global answer
     for d1 in range(1, max_d1 + 1):
         for d2 in range(1, max_d2 + 1):
             election_districts = [0, 0, 0, 0, 0]
@@ -16,17 +16,17 @@ def calc_gerrymandering(r, c, max_d1, max_d2):
                     elif (0 <= row <= r + d2 and c < col <= N - 1) and col - row > c - r:
                         election_districts[1] += city_map[row][col]
                     # No.3 election_district
-                    elif (r + d1 <= row <= N - 1 and 0 <= col < c - d1 + d2) and ((row - col) > (r + d1) - (c - d1)):
+                    elif (r + d1 <= row <= N - 1 and 0 <= col < c - d1 + d2) and (row - col) > (r + d1) - (c - d1):
                         election_districts[2] += city_map[row][col]
                     # No.4 election_district
-                    elif (r + d2 < row <= N - 1 and c - d1 + d2 <= col <= N - 1) and ((row + col) > (r + d1 + d2) + (c - d1 + d2)):
+                    elif (r + d2 < row <= N - 1 and c - d1 + d2 <= col <= N - 1) and (row + col) > (r + d1 + d2) + (c - d1 + d2):
                         election_districts[3] += city_map[row][col]
                     # No.5 election_district
                     else:
                         election_districts[4] += city_map[row][col]
                 now_diff = max(election_districts) - min(election_districts)
-                temp_answer = min(now_diff, temp_answer)
-    return temp_answer
+                if now_diff < answer:
+                    answer = now_diff
 
 
 def check_boundary_length(r, c):
@@ -47,6 +47,6 @@ answer = 0xffffff
 for r in range(N - 2):
     for c in range(1, N - 1):
         max_d1, max_d2 = check_boundary_length(r, c)
-        answer = min(answer, calc_gerrymandering(r, c, max_d1, max_d2))
+        calc_gerrymandering(r, c, max_d1, max_d2)
 
 print(answer)
